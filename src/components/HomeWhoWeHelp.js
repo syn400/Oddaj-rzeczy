fb35d761e93669be7a33431e8ab1468eac0cc50c
Konrad <synowiec400@wp.pl>
2020-11-09 08:43:02 +0100

    Dodanie logiki do sekcji 'Komu pomagamy' oraz podpięcie firebase do projektu

--- src/components/HomeWhoWeHelp.js
+++ src/components/HomeWhoWeHelp.js
@@ -1,20 +1,37 @@
 import '../scss/HomeWhoWeHelp.scss';
 import decoration from '../assets/Decoration.svg';
+import {useState} from 'react';
 
 export const HomeWhoWeHelp = () => {
+    const [content, setContent] = useState('Foundations');
+
+    const showParagraph = () => {
+        if(content === 'Organizations') {
+            return <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+        } else if (content === 'Local') {
+            return <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+        } else {
+            return <p>W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.</p>
+        }
+    }
+
     return (
         <section id='organizations--section'>
             <div className='organizations--container'>
                 <h2>Komu pomagamy?</h2>
                 <img src={decoration} alt='Dekoracja' />
                 <div className='btn--container'>
-                    <button className='active'>Fundacjom</button>
-                    <button>Organizacjom<br/>pozarządowym</button>
-                    <button>Lokalnym<br/>zbiórkom</button>
+                    <button 
+                        className={content === 'Foundations' ? 'active' : null} 
+                        onClick={()=>setContent('Foundations')}>Fundacjom</button>
+                    <button
+                        className={content === 'Organizations' ? 'active' : null} 
+                        onClick={()=>setContent('Organizations')}>Organizacjom<br/>pozarządowym</button>
+                    <button
+                        className={content === 'Local' ? 'active' : null} 
+                        onClick={()=>setContent('Local')}>Lokalnym<br/>zbiórkom</button>
                 </div>
-                <p>
-                    W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.
-                </p>
+                {showParagraph()}
                 <ul className='result--list'>
                     <li>
                         <div>
---
+++ src/fire.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+const config = {
+    apiKey: "AIzaSyBVD7Wb91ssb0LElRi8bk9M4FoX6HogQKc",
+    authDomain: "oddaj-rzeczy-51076.firebaseapp.com",
+    databaseURL: "https://oddaj-rzeczy-51076.firebaseio.com",
+    projectId: "oddaj-rzeczy-51076",
+    storageBucket: "oddaj-rzeczy-51076.appspot.com",
+    messagingSenderId: "970682417103",
+    appId: "1:970682417103:web:5272a585e638fbfb881cba",
+    measurementId: "G-BSME8E2EH9"
+  };
+
+export default config;
